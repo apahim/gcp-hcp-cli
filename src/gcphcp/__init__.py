0463d4f7f68f0432b0eeb0068f519f096a92c24c
Amador Pahim <apahim@redhat.com>
2025-10-28 15:06:06 +0000

    Prep 0.1.2
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- src/gcphcp/__init__.py
+++ src/gcphcp/__init__.py
@@ -4,7 +4,7 @@ This package provides a comprehensive CLI for managing clusters and nodepools
 through the GCP HCP API, following gcloud CLI conventions.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = "Red Hat GCP HCP Team"
 __email__ = "hcm-gcp-hcp@redhat.com"
 
